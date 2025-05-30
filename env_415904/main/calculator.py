7cd6c9795fa961ab981c1795dbdb2bac5ec895d9
patrysUwU <patryk.fryzlewicz@gmail.com>
2025-05-30 23:12:04 +0200

    test testow

--- env_415904/main/calculator.py
+++ env_415904/main/calculator.py
@@ -1,30 +1,16 @@
 import numpy as np
-import pandas as pd
-
 
 def dodawanie(liczba1, liczba2):
     return np.add(liczba1, liczba2)
 
-
 def odejmowanie(liczba1, liczba2):
     return np.subtract(liczba1, liczba2)
 
-
 def mnozenie(liczba1, liczba2):
     return np.multiply(liczba1, liczba2)
-
 
 def dzielenie(liczba1, liczba2):
     if liczba2 != 0:
         return np.divide(liczba1, liczba2)
     else:
         return "Błąd: dzielenie przez zero!"
-
-
-def isna(val):
-    return pd.isna(val)
-
-
-def add_column(df):
-    df["sum"] = df["a"] + df["b"]
-    return df
